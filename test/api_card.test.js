6774c4aa548074fb271e9e0c01cb07a5584f876b
Guan Bo <guanbo2002@gmail.com>
2016-08-31 11:08:27 +0800

    Fixed: getSubmerchant

--- test/api_card.test.js
+++ test/api_card.test.js
@@ -17,17 +17,17 @@ describe('api_card', function () {
     });
   });
   
-  describe.skip('submitSubmerchant', function(){
-    var options = {
-     "brand_name": "aaaaaa",
-      // "app_id":"xxxxxxxxxxx",
-      "logo_url": "http://mmbiz.qpic.cn/mmbiz/O1DymY4NpO88CjYk0XWw9VAW99RMibqchv2OVDOibPpmMu65H47usx4fjyRwvRaZwCccibCiccMgwPk9unibewSQfjw/0?wx_fmt=jpeg",
-      "protocol": "xxxxxxxxxx",
-      "end_time": Math.round(Date.now()/1000),
-      "primary_category_id": 1,
-      "secondary_category_id": 101
-    };
-    
+  var options = {
+   "brand_name": "aaaaaa",
+    // "app_id":"xxxxxxxxxxx",
+    "logo_url": "http://mmbiz.qpic.cn/mmbiz/O1DymY4NpO88CjYk0XWw9VAW99RMibqchv2OVDOibPpmMu65H47usx4fjyRwvRaZwCccibCiccMgwPk9unibewSQfjw/0?wx_fmt=jpeg",
+    "protocol": "xxxxxxxxxx",
+    "end_time": Math.round(Date.now()/1000+3600*24*365),
+    "primary_category_id": 1,
+    "secondary_category_id": 101
+  };
+  
+  describe('submerchant', function(){
     before(function(done){
       api.uploadMedia('./test/fixture/image.jpg', 'image', function (err, data, res) {
         options.protocol = data.media_id;
@@ -35,13 +35,50 @@ describe('api_card', function () {
       });
     });
     
-    it('should ok', function(done){
-      api.submitSubmerchant(options, function (err, data, res) {
-        expect(err).to.not.be.ok();
-        expect(data).to.have.property('info');
-        expect(data.info).to.have.property('merchant_id');
-        done();
+    describe('submit', function(){
+      it('should ok', function(done){
+        api.submitSubmerchant(options, function (err, data, res) {
+          expect(err).to.not.be.ok();
+          expect(data).to.have.property('info');
+          expect(data.info).to.have.property('merchant_id');
+          done();
+        })
       })
-    })
-  })
+    });
+  
+    describe('update', function(){
+      it('should ok', function(done){
+        options.merchant_id = options.merchant_id||408592871;
+        api.updateSubmerchant(options, function (err, data, res) {
+          expect(err).to.not.be.ok();
+          expect(data).to.have.property('info');
+          expect(data.info).to.have.property('merchant_id', options.merchant_id);
+          done();
+        });
+      });
+    });
+    
+    describe('get', function(){
+      it('should ok', function(done){
+        options.merchant_id = options.merchant_id||408592871;
+        api.getSubmerchant(options.merchant_id, function (err, data, res) {
+          expect(err).to.not.be.ok();
+          expect(data).to.have.property('info');
+          expect(data.info).to.have.property('merchant_id', options.merchant_id);
+          done();
+        });
+      });
+    });
+    
+    describe('batchget', function(){
+      it('should ok', function(done){
+        api.batchgetSubmerchant({"begin_id": 0,"limit": 50}, function (err, data) {
+          expect(err).to.not.be.ok();
+          expect(data).to.have.property('info_list');
+          expect(data.info_list[0]).to.have.property('merchant_id', options.merchant_id);
+          done();
+        });
+      })
+    });
+  });
 });
